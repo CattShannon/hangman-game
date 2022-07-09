6aa6404c5fe4d586125bb4857136337ff13f9df9
Sebastian Aguirre <sebastian.aguirre@sofka.com.co>
2022-07-08 22:23:12 -0500

    Se crean los dibujos ascii del titulo del juego y las escenas de hangman

---
+++ main.py
@@ -0,0 +1,28 @@
+import random
+import math
+import hangman_draws
+import os
+
+
+def read_users_file():
+    pass
+
+
+def user_sing_up(): 
+    pass
+
+
+def user_login():
+    pass    
+
+
+def read_words_file():
+    pass
+
+
+def run():
+    pass
+
+if __name__ == '__main__':
+    run()
+
